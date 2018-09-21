fcf3ecc0f9571445bcbfba3e57c9fc38677113d4
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-20 19:06:40 -0500

    e8 finished

--- LINQedLists/Program.cs
+++ LINQedLists/Program.cs
@@ -27,7 +27,7 @@ namespace LINQedLists
             //QUERY SYNTAX
             //var fourSixMultiples =
             //    from number in numbers
-            //    where (number % 2 == 0) == true
+            //    where (number % 4 == 0 || number % 6 == 0) == true
             //    select number;
 
             //foreach (int num in numbers)
@@ -113,18 +113,43 @@ namespace LINQedLists
             //------------END OF E6---------------
 
             //What is our most expensive product?
-            List<double> prices = new List<double>()
-            {
-                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
-            };
+            //List<double> prices = new List<double>()
+            //{
+            //    879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            //};
 
-            var mostExpensive = prices.Max();
+            //var mostExpensive = prices.Max();
+            //{
+            //    Console.WriteLine($"our highest priced item is: {mostExpensive}");
+            //}
+
+            //Console.Read();
+            //---------END E7--------------
+
+    //        //PARTITIONING OPERATIONS 
+    //          Store each number in the following List until a perfect square
+                //is detected.
+
+                //Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+
+            List<int> wheresSquaredo = new List<int>()
+                {
+                    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+                };
+
+            List<int> notSquared = wheresSquaredo.Where(n => Math.Sqrt(n) % 1 == 0).ToList();
+            foreach (var num in notSquared)
             {
-                Console.WriteLine($"our highest priced item is: {mostExpensive}");
+                Console.WriteLine(num.ToString());
             }
+            
+
+            Console.WriteLine();
 
             Console.Read();
         }
+
+
     }
 
     
