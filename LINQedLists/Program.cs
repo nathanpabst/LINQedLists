438b1ea530702a2fe7bb139ce36a049b8ca56a69
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-20 19:44:00 -0500

    millionaires printing

--- LINQedLists/Program.cs
+++ LINQedLists/Program.cs
@@ -126,31 +126,58 @@ namespace LINQedLists
             //Console.Read();
             //---------END E7--------------
 
-    //        //PARTITIONING OPERATIONS 
-    //          Store each number in the following List until a perfect square
-                //is detected.
+            //        //PARTITIONING OPERATIONS 
+            //          Store each number in the following List until a perfect square
+            //is detected.
 
-                //Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            //List<int> wheresSquaredo = new List<int>()
+            //    {
+            //        66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            //    };
 
-            List<int> wheresSquaredo = new List<int>()
-                {
-                    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
-                };
+            //List<int> findSquares = wheresSquaredo.Where(n => Math.Sqrt(n) % 1 == 0).ToList();
+            //foreach (var num in findSquares)
+            //{
+            //    Console.WriteLine(num.ToString());
+            //}
 
-            List<int> notSquared = wheresSquaredo.Where(n => Math.Sqrt(n) % 1 == 0).ToList();
-            foreach (var num in notSquared)
-            {
-                Console.WriteLine(num.ToString());
-            }
-            
+            //Console.Read();
+            //------------END E8------------------
 
+            // Build a collection of customers who are millionaires
+
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            var richPeeps = from cust in customers
+                            where cust.Balance >= 1000000
+                            select cust.Name;
+            Console.WriteLine("Millionaires club:");
+            Console.WriteLine(string.Join(", ", richPeeps));
             Console.WriteLine();
 
             Console.Read();
+
+
         }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
+    }
 
 
     }
 
-    
-}
